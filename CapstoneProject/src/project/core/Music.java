a16cffce8c6eac8eb2bf2d6093ed9fb4c9776a0c
Alan Ma <ama386@student.fuhsd.org>
2022-05-11 06:40:41 -0700

    Added sounds (need to fix)

---
+++ CapstoneProject/src/project/core/Music.java
@@ -0,0 +1,9 @@
+package project.core;
+
+import jay.jaysound.*;
+
+public class Music {
+	public JayLayer player;
+	
+	private ArrayList<String> music = 
+}
---
+++ CapstoneProject/src/project/core/Sounds.java
@@ -0,0 +1,64 @@
+package project.core;
+
+import project.core.Content.*;
+
+import gameutils.struct.Seq;
+import jay.jaysound.JayLayer;
+
+public class Sounds implements ContentList{
+	public JayLayer layer;
+	
+	public static Seq<SoundEffect> effects = new Seq<>();
+	public static Seq<Music> songs = new Seq<>();
+	
+	public static SoundEffect test;
+	public static Music music;
+	
+	public void load() {
+//		test = new SoundEffect("bruh");
+		music = new Music("Chicken.mp3");
+		layer=new JayLayer("assets/audio/music/","assets/audio/effects/",false);
+//		layer.addPlayList();
+//		layer.addSongs(0,songs);
+//		String[] tmp = new String[effects.size];
+//		for(int i = 0;i < effects.size;i++) tmp[i] = effects.get(i).name;
+//		layer.addSoundEffects(tmp);
+		
+//		tmp = new String[songs.size];
+//		for(int i = 0;i < songs.size;i++) tmp[i] = songs.get(i).name;
+		layer.addPlayList();
+		layer.addSongs(0, new String[] {"Chicken.mp3","Chicken2.mp3","Chicken3.mp3"});
+//		layer.nextSong();
+		layer.changePlayList(0);
+		layer.nextSong();
+//		layer.changePlayList(0);
+//		layer.addJayLayerListener(this);
+	}
+	
+	
+public class SoundEffect{
+	public String name;
+	public int id;
+	
+	public SoundEffect(String name) {
+		id = effects.size;
+		effects.add(this);
+		this.name = name;
+	}
+	
+	public void play() {
+		layer.playSoundEffect(id);
+	}
+}
+
+public class Music{
+	public String name;
+	public int id;
+	
+	public Music(String name) {
+		id = songs.size;
+		songs.add(this);
+		this.name = name;
+	}
+}
+}
