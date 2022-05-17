e60703a4e11a323162655e8db60496de2c304290
Jerry Yang <89053584+jyang498@users.noreply.github.com>
2022-05-17 10:03:08 -0700

    Better Lose screen

--- CapstoneProject/src/project/ui/screens/LoseScreen.java
+++ CapstoneProject/src/project/ui/screens/LoseScreen.java
@@ -3,37 +3,33 @@ package project.ui.screens;
 import gameutils.func.*;
 import project.core.UI.*;
 import project.graphics.*;
-import project.ui.List;
 import project.ui.*;
-import project.ui.interactables.Button;
-
-import java.awt.*;
+import project.ui.interactables.*;
 
 import static java.lang.Math.*;
 import static project.Vars.*;
 
 public class LoseScreen extends Screen{
     public Table side;
-    public float sidex = 0;
-    public Cons<Button> buttonHover = b -> Effects.blur.draw(-50, 0, 350, b.height(), Pal.opaqueWhite);
+    public Cons<Button> buttonHover = b -> {
+        Effects.blur.draw(100, 0, 125, b.height(), Pal.opaqueWhite);
+        Effects.blur2.draw(-25, 0, 125, b.height(), Pal.opaqueWhite);
+    };
 
     @Override
     public void rebuild(){
         side = new Table(table -> table.add(new List(list -> {
-            list.text(text -> text.text("G A M E  O V E R").size(100).y(100).alignX(AlignX.center).color(Pal.opaqueWhite));
+            list.text(text -> text.text("G A M E  O V E R").size(100).x(100).y(100).alignX(AlignX.center).color(Pal.opaqueWhite));
             list.row(20);
             list.button(button -> {
                 button.hover = buttonHover;
-                button.press(b -> canvas.screen(ui.menuScreen)).text(text -> text.text("PLAY AGAIN").size(35).color(Pal.opaqueWhite)).width(150);
+                button.press(b -> canvas.screen(ui.menuScreen)).text(text -> text.text("PLAY AGAIN").size(35).x(100).alignX(AlignX.center).color(Pal.opaqueWhite)).width(150);
             }).alignX(AlignX.center);
-        }))).x(width/2f).y(10);
+        }))).x(width / 2f - 100).y(10);
     }
 
     @Override
     public void update(){
-        float wanted = input.mouse.x < 200 ? 200 : 0;
-        sidex += (wanted - sidex) / 10;
-
         ui.gameScreen.update();
         delta = max(delta - 0.01f, -100000);
     }
@@ -45,10 +41,8 @@ public class LoseScreen extends Screen{
         canvas.tint(255, 255, 255);
         ui.gameScreen.draw();
         canvas.fill(0, 0, 0, 100);
-        canvas.rect(0, 0, width, height);
+        canvas.rect(0, 0, width + 20, height + 50);
         canvas.popMatrix();
-
-        Effects.blur.draw(sidex - 300, 0, 300, height, Color.black);
 
         side.process();
     }
